bddf20dc14b12ee4420d960fd74dca5e59d34abd
bleak69 <goldenguy004@gmail.com>
2026-07-14 06:11:09 +0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Love all",
+        name: "with her cat",
         // Activity type number (0 = Playing).
-        type: 2,
+        type: 0,
       },
     ],
   },
